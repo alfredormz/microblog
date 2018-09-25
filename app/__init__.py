5cb1fae8c14427875928b23036f7d4240ff43511
Nicolás Cravetz <ncravetz@gmail.com>
2018-09-25 17:05:54 -0300

    The Flask Mega-Tutorial Part III: Web Forms
    Added Login function and forms.
    Added config.py
    Added WTF module

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-# __init__.py - Flask application instance
+# app/__init__.py - Flask application instance
 
 from flask import Flask
+from config import Config 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
 
