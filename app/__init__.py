00eeb35a31cf871779240be7cb70d34ff95f215f
Nicolás Cravetz <ncravetz@gmail.com>
2018-10-01 10:08:41 -0300

    The Flask Mega-Tutorial Part VI: Profile Page and Avatars
    Profile Pages
    Avatars
    Post sub template
    Last seen 
    About me

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
-# app/__init__.py - Flask application instance
-
 from flask import Flask
-from config import Config 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,8 +11,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-
 from app import routes, models
-
-
-
