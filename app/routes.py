5e5f1d5b74f94bf231f45f1edcc26d0e7a743d22
Nicolas Cravetz <ncravetz@gmail.com>
2018-11-05 18:09:31 -0300

    The Flask Mega-Tutorial Part XIV: Ajax - Added live translation service to posts

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g 
+from flask import render_template, flash, redirect, url_for, request, g, jsonify 
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
@@ -8,6 +8,7 @@ from app.models import User, Post
 from app.email import send_password_reset_email
 from flask_babel import _, get_locale
 from guess_language import guess_language
+from app.translate import translate
 
 
 
@@ -194,6 +195,20 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form) 
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
+
+
+
+
+
+
 
 
 
